e8af0a599519cd4da6f1c385ecab3aff9a81a7af
a <85395885+ArconRM@users.noreply.github.com>
2024-09-21 13:49:31 +0400

    Completed task 4

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,14 +1,14 @@
 package org.knit;
 
-import org.knit.lab1.Task1;
-import org.knit.lab1.Task2;
+//import org.knit.lab1.Task1;
+//import org.knit.lab1.Task2;
+
+
+import org.knit.lab2.Task4;
 
 public class Main {
     public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        task1.execute();
-
-        Task2 task2 = new Task2();
-        task2.execute();
+        Task4 task4 = new Task4();
+        task4.execute();
     }
 }
