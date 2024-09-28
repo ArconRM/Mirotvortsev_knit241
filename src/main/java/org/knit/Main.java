2f744a31422bd2db13372c8e86c67eaca768077a
a <85395885+ArconRM@users.noreply.github.com>
2024-09-28 13:43:37 +0400

    Completed lab 3 (only task 6)

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,12 +1,31 @@
 package org.knit;
 
-import org.knit.lab1.Task1;
-import org.knit.lab1.Task2;
-import org.knit.lab2.Task5;
+import org.knit.lab3.Mage;
+import org.knit.lab3.PlayerFabric;
+import org.knit.lab3.Priest;
+import org.knit.lab3.WarriorTank;
 
 public class Main {
     public static void main(String[] args) {
-        Task5 task5 = new Task5();
-        task5.execute();
+        PlayerFabric fabric = new PlayerFabric();
+
+        WarriorTank warrior = fabric.createWarriorTank("Some Ork");
+        warrior.move(10, 10);
+
+        Mage mage = fabric.createMage("Some Mage");
+        mage.move(10, 20);
+
+        Priest priest = fabric.createPriest("Some Healer");
+        priest.move(10, 20);
+
+        warrior.attack(mage);
+        priest.heal(mage);
+        mage.castSpell(warrior);
+        mage.castSpell(warrior);
+        mage.castSpell(warrior);
+        mage.castSpell(warrior);
+        mage.castSpell(warrior);
+
+        System.out.println(warrior);
     }
 }
